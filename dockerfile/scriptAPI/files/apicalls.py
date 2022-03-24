48b8382a7d8f66a9275726d327ace6175a3f5329
Sénéchal Julien <35048771+Teckinfor@users.noreply.github.com>
2022-03-24 09:38:34 +0100

    Update apicalls.py

--- dockerfile/scriptAPI/files/apicalls.py
+++ dockerfile/scriptAPI/files/apicalls.py
@@ -1,6 +1,6 @@
 from elastic_enterprise_search import AppSearch
 from files.sorter import *
-import requests, datetime, xmltodict, json
+import requests
 
 NO_TOKEN = {"Error":"Impossible to get an authorization"}
 
@@ -17,14 +17,6 @@ def get_authorization(config:dict) :
         return NO_TOKEN
 
     return authorization_json["access_token"]  
-    
-def get_credits(config:dict, token:str) :
-    headers = {"Authorization": "Bearer " + token}
-    url = "https://management.azure.com/providers/Microsoft.Billing/billingAccounts/"+ config["subscriptions"]["BillingAccountID"] +"/billingProfiles/" + config["subscriptions"]["BillingProfileID"] + "/providers/Microsoft.Consumption/credits/balanceSummary?api-version=2021-10-01"
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        response_json = response.json()
-        return response_json
 
 def get_details_bill(config:dict, token:str) :
     headers = {"Authorization": "Bearer " + token}
@@ -33,34 +25,6 @@ def get_details_bill(config:dict, token:str) :
     if response.status_code == 200:
         response_json = response.json()
         return response_json
-
-def list_containers_in_blobs(config:dict) :
-
-    headers = {"x-ms-version":"2020-04-08", "x-ms-date":datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}
-    array = []
-    response_dict = {}
-
-    for account in config["subscriptions"]["StorageAccount"].keys():
-        array.append(account)
-
-    for blob in array :
-        url = "https://"+ blob +".blob.core.windows.net/"+ config["subscriptions"]["StorageAccount"][blob]+"&comp=list"
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            content = xmltodict.parse(response.content)
-            temp = []
-
-            if isinstance(content["EnumerationResults"]["Containers"]["Container"], list) :
-
-                for container in content["EnumerationResults"]["Containers"]["Container"] :
-                    temp.append(container["Name"])
-            
-            else :
-                temp.append(content["EnumerationResults"]["Containers"]["Container"]["Name"])
-
-            response_dict[blob] = temp
-
-    return response_dict
     
 def send_data_to_elasticsearch(config:dict, engine:str, files:list):
     
@@ -72,4 +36,3 @@ def send_data_to_elasticsearch(config:dict, engine:str, files:list):
     except :
         return "Indexing failed"
     return "Ok"
-
